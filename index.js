b0497dd7f65a083631dd6cb5442afb2d6a56ed54
Olloshukur6092 <matnazarovolloshukur7223@gmail.com>
2024-12-28 12:04:59 +0500

    added new function

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ bot.command("mevalar", (ctx) => {
   ctx.reply("Bu yerda sizga mevalar bor bo'ladi!!!");
 });
 
+bot.on(":text", (ctx) => {
+  let text = reverseMessage(ctx.message.text);
+
+  ctx.reply(text, {
+    reply_parameters: { message_id: ctx.msg.message_id },
+  });
+});
+
+function reverseMessage(messageUsers) {
+  let message = "";
+  for (let i = messageUsers.length - 1; i >= 0; i--) {
+    message += messageUsers[i];
+  }
+
+  return message;
+}
+
 bot.start();
